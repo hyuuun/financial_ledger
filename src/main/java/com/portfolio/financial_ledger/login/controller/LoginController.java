61091814ed72f991be94ad5c94187a53ba9f6aba
hyun <meteor139@naver.com>
2023-03-30 23:41:25 +0900

    비밀번호 틀렸을 때 안내 문구 추가

--- src/main/java/com/portfolio/financial_ledger/login/controller/LoginController.java
+++ src/main/java/com/portfolio/financial_ledger/login/controller/LoginController.java
@@ -5,7 +5,10 @@ import com.portfolio.financial_ledger.setting.dto.SettingDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller()
@@ -17,6 +20,13 @@ public class LoginController {
     @GetMapping("/login")
     public String loginPage() {
         return "login/login";
+    }
+
+    @GetMapping("/login/error")
+    public String loginFail(Model model) {
+        model.addAttribute("err_msg", "비밀번호를 확인해주세요.");
+
+        return "/login/login";
     }
 
     @GetMapping("/logout")
